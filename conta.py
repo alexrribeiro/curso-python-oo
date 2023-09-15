6ce9314b79066a1d4c869565c14a82b8492be732
Alex R Ribeiro <alexramosribeiro@gmail.com>
2023-09-15 00:26:41 -0300

    A5 Final

--- conta.py
+++ conta.py
@@ -1,4 +1,5 @@
 class Conta:
+
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto... {}".format(self))
         self.__numero = numero
@@ -18,3 +19,19 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
